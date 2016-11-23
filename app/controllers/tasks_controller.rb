3f8e5a5ba7068866bacc5d2ba527614af185c3a4
Hugh Mitton <hm@hughmitton.com>
2016-11-23 09:41:36 +0100

    finally managed tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,26 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :update, :edit, :destroy]
+
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      #back to the form
+    end
   end
 
   def edit
@@ -18,5 +30,16 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,33 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  root 'tasks#index'
+  resources :tasks
+# #show all tasks
+#   get 'tasks', to: 'tasks#index'
 
-  get 'tasks/show'
+# #show one task
 
-  get 'tasks/new'
+#   get 'tasks/:id', to: 'tasks#show'
 
-  get 'tasks/create'
+# #create new restaurant
+#   get 'tasks/new', to: 'tasks#new'
 
-  get 'tasks/edit'
+#   post 'tasks', to: 'tasks#create'
 
-  get 'tasks/update'
+# #update a task
+#   get 'tasks/:id/edit', to: 'tasks#edit'
 
-  get 'tasks/destroy'
+#   patch 'tasks/:id', to: 'tasks#update'
+
+# #delete a task
+#   delete 'tasks/:id', to: 'tasks#destroy'
+
+#   GET '/tasks': get all your tasks.
+# GET '/tasks/:id': get a precise task, e.g GET '/tasks/33' get task with id=3
+# GET '/tasks/new': get the form to create a new task
+# POST '/tasks': post a new task
+# GET '/tasks/:id/edit': get the form to edit an existing task
+# PATCH '/tasks/:id': update an existing task
+# DELETE '/tasks/:id': delete an existing task
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
